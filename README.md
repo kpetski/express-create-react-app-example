7f0a720ab7174bca37324da7d8b2c90c906ae3d3
kpetski <keripetski@hotmail.com>
2017-11-16 19:05:50 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # express-create-react-app-example
-create-react-app with express
+- create-react-app with express. Following tutorial as base. References below
+
+### walk through code
+https://github.com/mars/heroku-cra-node
+
+### walk through website
+http://ericsowell.com/blog/2017/5/16/create-react-app-and-express
---
+++ react-ui/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: null,
+      fetching: true
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        this.setState({
+          message: json.message,
+          fetching: false
+        });
+      }).catch(e => {
+        this.setState({
+          message: `API call failed: ${e}`,
+          fetching: false
+        });
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      <p className="App-intro">
+        {this.state.fetching
+          ? 'Fetching message from API'
+          : this.state.message}
+      </p>
+      </div>
+    );
+  }
+}
+
+export default App;
